229267842b66777b796b996527d791f218e44316
Justinus Andjarwirawan <justinus@gmail.com>
2020-06-17 08:00:14 +0700

    Initial Commit

---
+++ MyTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTable
+//
+//  Created by Justinus Andjarwirawan on 17/06/20.
+//  Copyright © 2020 Justinus Andjarwirawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
